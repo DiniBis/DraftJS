04fb6c9238cb0330cf65bc3a22f045f7ff8e8d99
DiniBis <106832577+DiniBis@users.noreply.github.com>
2024-01-26 20:21:28 +0100

    Liaison avec le JSON local + Fin de l'affichage J1 + Clique->YDK

--- src/index.js
+++ src/index.js
@@ -1,61 +1,77 @@
 //Connexion au Json local
-fetch('yugioh-cube.json')
-    .then(response => response.json())
-    .then(data => {
-    console.log(data)
-    })
-    .catch(error =>
-        console.error('Erreur json local',error))
+/* let dataCartes=[] */
 
-
-//Exemple de requête avec l'id de Head Huntress
-async function loadData() {
-    try {
-        let carteId = 4031928
-        let jsonDataRequest = await fetch('https://db.ygoprodeck.com/api/v7/cardinfo.php?id='+carteId+'&language=fr') //id remplaçable selon la carte
-        let jsonData = await jsonDataRequest.json()
-        console.log(jsonData)
-        return jsonData
-    } catch (err) {
-        console.log(`Une erreur s'est produite`, err)
-    }
+async function loadDataLocal() {
+  let jsonDataLocalRequest = await fetch('yugioh-cube.json')
+  let jsonDataLocal = await jsonDataLocalRequest.json()
+  return jsonDataLocal
 }
 
-//Remplacer l'image
-const setupImg1 = document.getElementById('carte1Image')
-document.addEventListener("DOMContentLoaded", async () => {
-    let imageTest = await loadData()
-    const monimageTest = document.getElementById('carte1Image')
-    monimageTest.setAttribute("src", (imageTest.data[0].card_images[0].image_url))
-})
+//Lecture du JsonLocal + Appel à l'affichage via l'API + Clique->ydk
+document.addEventListener('DOMContentLoaded', async () => {
+  let dataCartes2 = await loadDataLocal()
+  /* dataCartes.push(dataCartes2) */
+  const randomIndex = Math.floor(Math.random() * dataCartes2.length)
+  const randomCardId = dataCartes2[randomIndex].id
+  await loadData(randomCardId)
 
-//Remplacer la description
-const setupDesc1 = document.getElementById('carte1Description')
-document.addEventListener("DOMContentLoaded", async () => {
-    let descTest = await loadData()
-    setupDesc1.textContent=(descTest.data[0].desc)
-})
+  //Au clique de la carte, l'ajoute au ydk du joueur
+  const carte1Image = document.getElementById('carte1Image')
+  carte1Image.addEventListener('click', () => {
+    const ydkJ1 = document.getElementById('ydkJ1')
+    const li = document.createElement('li')
+    li.textContent = randomCardId
+    ydkJ1.appendChild(li)
+    })
+});
 
-//Remplacer le nom
-const setupNom1 = document.getElementById('carte1Nom')
-document.addEventListener("DOMContentLoaded", async () => {
-    let Test = await loadData()
-    setupNom1.textContent=(Test.data[0].name)
-})
+// Affichage via requêtes à l'API selon l'id du json local
+async function loadData(carteId) {
+  try {
+    let jsonDataRequest=await fetch(`https://db.ygoprodeck.com/api/v7/cardinfo.php?id=${carteId}&language=fr`);
+    let jsonData=await jsonDataRequest.json();
 
-//Remplacer le type
-const setupType1 = document.getElementById('carte1Type')
-document.addEventListener("DOMContentLoaded", async () => {
-    let Test = await loadData()
-    setupType1.textContent=('Type: '+Test.data[0].type)
-})
+    // Remplacer l'image
+    const monimageTest = document.getElementById('carte1Image');
+    monimageTest.setAttribute('src',jsonData.data[0].card_images[0].image_url);
 
-//Remplacer le Niveau
-const setupNiveau1 = document.getElementById('carte1Niveau')
-document.addEventListener("DOMContentLoaded", async () => {
-    let Test = await loadData()
-    setupNiveau1.textContent=('Niveau: '+Test.data[0]?.level)
-})
+    //Remplacer la description
+    const setupDesc1 = document.getElementById('carte1Description')
+    setupDesc1.textContent=(jsonData.data[0].desc)
+
+    //Remplacer le nom
+    const setupNom1 = document.getElementById('carte1Nom')
+    setupNom1.textContent=(jsonData.data[0].name)
+
+    //Remplacer le type
+    const setupType1 = document.getElementById('carte1Type')
+    setupType1.textContent=('Type: '+jsonData.data[0].type)
+
+    //Remplacer le niveau
+    const setupNiveau1 = document.getElementById('carte1Niveau')
+    setupNiveau1.textContent=('Niveau: '+jsonData.data[0]?.level)
+
+    //Remplacer ATK/DEF
+    const setupATKDEF1 = document.getElementById('carte1ATKDEF')
+    setupATKDEF1.textContent=('ATK/ '+jsonData.data[0]?.atk+' DEF/ '+jsonData.data[0]?.def)
+  
+    //Remplacer l'attribut
+    const setupAttribut1 = document.getElementById('carte1Attribut')
+    setupAttribut1.textContent=('Attribut: '+jsonData.data[0]?.attribute)
+
+    //Remplacer race
+    const setupRace1 = document.getElementById('carte1Race')
+    setupRace1.textContent=('Race: '+jsonData.data[0]?.race)
+
+  }
+
+  catch (err) {
+    console.error(`Une erreur s'est produite`, err);
+  }
+}
+
+carte1=getElementById('carte1')
+carte1.addEventListener('click')
 
 /*
 Boucle de jeu
